82efa187c5d24c61301d4ce99a27240642c36073
gvpm <gvillar@id.uff.br>
2016-05-30 19:57:17 +0100

    all

---
+++ src/Sketch.java
@@ -0,0 +1,30 @@
+
+import processing.core.*;
+
+public class Sketch extends PApplet {
+
+    public static void main(String[] args) {
+
+        PApplet.main(new String[]{"Sketch"});
+    }
+
+    @Override
+    public void settings() {
+        size(512, 512);
+    }
+
+    @Override
+    public void setup() {
+         background(0);
+
+    }
+
+    @Override
+    public void draw() {
+         stroke(255);
+    if (mousePressed) {
+      line(mouseX,mouseY,pmouseX,pmouseY);
+    }
+
+    }
+}
