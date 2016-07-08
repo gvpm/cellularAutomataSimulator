138d4dfabec9cab14f6150aa5028dd057b6415be
gvpm <gvillar@id.uff.br>
2016-07-08 16:54:22 +0100

    Rule class with apply rule implemented, using the compare in cell

--- src/Rule.java
+++ src/Rule.java
@@ -10,4 +10,58 @@
  */
 public class Rule {
     
+    int type;
+    //1 for normal 2 for quantitative
+    
+    State newState;
+    
+    Cell ruleCell;
+    
+    //to use in quantitative rules
+    
+    State currentState;
+
+    public Rule(State newState, State currentState) {
+        type = 2;
+        
+        this.newState = newState;
+        this.currentState = currentState;
+        
+    }
+    //similarity case
+    public Rule(Cell ruleCell, State newState) {
+        type = 1;
+        this.newState = newState;
+        this.ruleCell = ruleCell;
+        if (!ruleCell.isRuleCell()){
+             throw new UnsupportedOperationException("Not a rule cell ");
+            
+        }
+    }
+    
+    public boolean applyRule(Cell c){
+        if(c.compare(ruleCell)){
+            State next = newState;
+            c.setNextState(next);
+            c.flagNewState();
+            
+            
+           return true; 
+        }else{
+        
+        
+            return false;
+        }
+    }
+    
+    
+    
+    
+    
+
+   
+    
+    
+    
+    
 }
